47f7916b78d4e3850e1b5606659fb0d3140b99fc
nisha783 <hm6980097@gmail.com>
2024-12-24 04:43:07 -0800

    update title

--- resources/views/card/create.blade.php
+++ resources/views/card/create.blade.php
@@ -4,7 +4,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Add New Card</title>
+  <title>{{ config('app.name') }}</title>
+  
 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
--- resources/views/card/edit.blade.php
+++ resources/views/card/edit.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Card</title>
+    <title>{{ config('app.name') }}</title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
